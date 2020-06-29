5007b2d9886727baff837fa13260ca2a8c569007
Tim Speelman <timspeelman@live.nl>
2020-06-29 14:17:45 +0200

    Modified deps

--- recipes/ipv8/__init__.py
+++ recipes/ipv8/__init__.py
@@ -14,15 +14,33 @@ class LocalIPV8Recipe(PythonRecipe):
 
     url = 'git+https://github.com/Tribler/py-ipv8.git'
 
-    depends = ['cryptography', 'libsodium', 'netifaces', 'pyasn1',
-               'python3', 'setuptools', 'twisted', 'lib2to3',
-               'libnacl', 'sqlite3', 'decorator', 'libnacl',
-               'six']
+    # A list of any other recipe names
+    # that must be built before this
+    # one
+    # https://github.com/kivy/python-for-android/tree/master/pythonforandroid/recipes
+    # [IPv8]: In https://github.com/Tribler/py-ipv8/blob/master/requirements.txt
+    # [sv]: In https://github.com/Tribler/ipv8-android-service/blob/jenkins_wild/recipes/ipv8/__init__.py
+    # [?]: Possibly unnecessary as not in sv, whilst in IPv8
+    depends = [
+                'aiohttp', # [IPv8][?] unknown
+                'aiohttp_apispec', # [IPv8][?] unknown
+                'asynctest', # [IPv8][?] unknown
+                'cryptography', # [IPv8][sv] https://github.com/kivy/python-for-android/tree/master/pythonforandroid/recipes/cryptography
+                'libnacl', # [IPv8][sv] unknown
+                'libsodium', # [sv] https://github.com/kivy/python-for-android/tree/master/pythonforandroid/recipes/libsodium
+                'netifaces', # [IPv8][sv] https://github.com/kivy/python-for-android/tree/master/pythonforandroid/recipes/netifaces
+                'pyasn1', # [IPv8][sv] unknown
+                'pyopenssl', # [IPv8] https://github.com/kivy/python-for-android/tree/master/pythonforandroid/recipes/pyopenssl
+                'python3', # [sv] https://github.com/kivy/python-for-android/tree/master/pythonforandroid/recipes/python3
+                'setuptools', # [sv] https://github.com/kivy/python-for-android/tree/master/pythonforandroid/recipes/setuptools
+                'lib2to3',  # [sv][local] unknown
+                'sqlite3', # [sv]
+                'decorator' # [sv]
+               ]
 
     patches = []
 
-   #  python_depends = ['hyperlink']
-    python_depends = ['hyperlink']
+    python_depends = ['hyperlink'] # [sv]
 
     site_packages_name = 'ipv8'
 
