38637e7122ccd0f5e0c9806bf89f8826de863e52
Tim Speelman <timspeelman@live.nl>
2020-06-30 10:41:35 +0200

    Make ipv8 only version

--- service/main.py
+++ service/main.py
@@ -1,27 +1,23 @@
-import logging
 import os
+import logging
 import sqlite3
 
 
-class OpenWalletService(object):
+class IPV8Service(object):
+
 
     def __init__(self):
         '''
         Setup environment
         '''
-        os.environ['PYTHON_EGG_CACHE'] = os.path.realpath(
-            os.path.join(os.getenv('ANDROID_PRIVATE'), '../cache'))
+        os.environ['PYTHON_EGG_CACHE'] = os.path.realpath(os.path.join(os.getenv('ANDROID_PRIVATE'), '../cache'))
 
         # Set logging format and level
-        logging.basicConfig(
-            format='%(levelname)s:%(message)s', level=logging.DEBUG)
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
 
     def run(self):
-        print("OpenWalletService run xoxo")
-        logging.warning("OpenWalletService run xoxo warn")
-
-
-
+        print("IPV8MAIN")
 
 if __name__ == '__main__':
-    OpenWalletService().run()
+    IPV8Service().run()
