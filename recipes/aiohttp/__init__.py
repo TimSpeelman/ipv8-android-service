988434d757ad9adc577c3a064d21cc3fab5bb39f
Tim Speelman <timspeelman@live.nl>
2020-06-30 14:05:43 +0200

    Add aiohttp deps

--- recipes/aiohttp/__init__.py
+++ recipes/aiohttp/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 class AioHTTPRecipe(CompiledComponentsPythonRecipe):
     version = '3.6.2'
     url = 'https://github.com/aio-libs/aiohttp/archive/v{version}.zip'
-    depends = ['setuptools', 'multidict']
+    depends = ['setuptools', 'multidict', 'attrs', 'chardet', 'yarl', 'async-timeout']
 
     site_packages_name = 'aiohttp'
     call_hostpython_via_targetpython = False
